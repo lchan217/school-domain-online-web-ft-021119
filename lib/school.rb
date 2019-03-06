1c6f1e14fa8a50a5bdcddcc3059c81b37ee5609f
Lizzie Chan <lchan217@gmail.com>
2019-03-06 15:07:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,6 +8,8 @@ class School
   end 
   
   def add_student(name, grade)
-    @roster[name] << grade
+    if #grade exists, just shovel naem in 
+    else #grade doesn't exist, create new grade
+    end 
   end 
 end 
