093601d539536bb63f106e1c1d2cc63ffa08de64
Lizzie Chan <lchan217@gmail.com>
2019-03-06 15:05:12 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,22 +7,5 @@ class School
     @roster = {}
   end 
   
-  def add_student(name, grade)
-    if @roster.include?(grade)
-      @roster[grade] << name 
-    else 
-      @roster[grade] = []
-      @roster[grade] << name 
-    end 
-  end 
   
-  def grade(grade)
-    @roster[grade]
-  end 
-  
-  def sort 
-    @roster.each do |grade, name|
-      @roster[grade] = name.sort 
-    end
-  end 
 end 
